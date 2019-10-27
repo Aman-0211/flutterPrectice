1bb8fd834c74700f97aaedc52209f4ff236b038e
Aman Thakur <amanthakur@ALIPL5251.local>
2019-10-28 00:18:12 +0530

    adding margin & padding to the image container & title

--- lib/src/widgets/imageList.dart
+++ lib/src/widgets/imageList.dart
@@ -9,8 +9,27 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Image.network(images[index].url);
+        return buildImage(images[index]);
       },
     );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+        padding: EdgeInsets.all(20.0),
+        margin: EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              child: Image.network(image.url),
+              padding: EdgeInsets.only(bottom: 8.0),
+            ),
+            Text(image.title)
+          ],
+        ));
   }
 }
